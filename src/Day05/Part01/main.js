28f3dd9ad494965234da502aad5b9c33f413ffb0
David Brad <davidbrad.015@gmail.com>
2021-12-05 07:23:54 -0500

    Day 05 - WIP

---
+++ src/Day05/Part01/main.js
@@ -0,0 +1,5 @@
+export default async function (input)
+{
+  let lineSegments = input.map(str => str.split(" => "));
+  return 0;
+}
---
+++ src/Day05/Part01/tests.js
@@ -0,0 +1,26 @@
+import { assertEquals, describe, it } from "../../lib/testing.js";
+
+import fn from "./main.js";
+
+export default async function ()
+{
+  describe("When testing a test", async () =>
+  {
+    it("should return true", async () =>
+    {
+      const input = [
+        "0,9 -> 5,9",
+        "8,0 -> 0,8",
+        "9,4 -> 3,4",
+        "2,2 -> 2,1",
+        "7,0 -> 7,4",
+        "6,4 -> 2,0",
+        "0,9 -> 2,9",
+        "3,4 -> 1,4",
+        "0,0 -> 8,8",
+        "5,5 -> 8,2",
+      ];
+      assertEquals(await fn(input), 0);
+    });
+  });
+}
